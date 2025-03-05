321cc84af83e0acc58dd0009e0d0a1ad57f40171
Udeet Mittal <udeet1575@gmail.com>
2025-03-05 21:01:48 +0530

    UI

--- app/page.tsx
+++ app/page.tsx
@@ -20,19 +20,29 @@ export default function Page() {
 
       <p className="mb-4">
         {`I'm `}
-        <span className="font-semibold">Udeet Mittal</span>
+        <span className="font-semibold" style={{
+      backgroundColor: '#9eff9e', // Light green background color
+      color: '#0f0f0f',           // Keep the text color unchanged
+      padding: '0 4px',           // Add padding for better appearance
+      borderRadius: '4px'         // Rounded edges similar to the image
+    }}>Udeet Mittal</span>
         {`, and this is my place on the internet.
          Your typical Indian software engineer by profession, but a not-so-typical one by passion.
-        I believe my laziness drives me to automate things, my curiosity drives me to learn new things, all at a fast pace that comes naturally to me,
+        I believe my laziness drives me to automate things, my curiosity drives me to learn new things, all at a fast pace with an intense focus on attention to detail,
     and I think this is what sets me apart from the crowd. `}
       </p>
       <p className="mb-4">
-        {`I've come to realise that the domain I work in doesn't really matter,
+        {`I've come to realise that the domain and specfic technologies I work in don't really matter,
      the problem statement in front of me and the people I work with do.`}
       </p>
       <p className="mb-4">
-        {`I'm currently looking for my next role as a`}
-        <span className="font-semibold"> Software Engineer</span>
+        {`I'm currently looking for my next role as a `}
+        <span className="font-semibold" style={{
+      backgroundColor: '#FFC78F', // Light green background color
+      color: '#0f0f0f',           // Keep the text color unchanged
+      padding: '0 4px',           // Add padding for better appearance
+      borderRadius: '4px'         // Rounded edges similar to the image
+    }}>Software Engineer</span>
         {`, having industry experience through 4 internships + 1 full-time role.
         Feel free to reach out at any of the socials below.`}
       </p>
