14ba60bbdb57fd466b6f3b28ff16d7c917db0c36
Udeet Mittal <udeet1575@gmail.com>
2025-03-24 23:19:26 +0530

    remove peerlist

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -224,17 +224,6 @@ export default function Footer() {
               className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
               rel="noopener noreferrer"
               target="_blank"
-              href="https://peerlist.io/stark"
-            >
-              <PeerlistIcon />
-              {/* <p className="ml-2 h-7">twitter</p> */}
-            </a>
-          </li>
-          <li>
-            <a
-              className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
-              rel="noopener noreferrer"
-              target="_blank"
               href="mailto:udeet1575@gmail.com"
             >
               <MailIcon />
