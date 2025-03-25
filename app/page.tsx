b37aaf9df96a46ff36eb27cfa95c4ab8f6dabcf5
Udeet Mittal <udeet1575@gmail.com>
2025-03-25 21:38:23 +0530

    icons+name change

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,24 @@ import CommentSection from "./components/CommentSection";
 import Footer from "./components/footer";
 import SpotifyTrack from "./components/now-playing";
 import { MapPin } from "lucide-react";
+import { BadgeCheck } from "lucide-react";
 
 export default function Page() {
   return (
     <>
       <section>
-        <h1 className="mb-4 text-2xl font-semibold tracking-tighter">Hey!</h1>
+        <span className="relative group inline-flex items-center gap-1 mb-4 text-xl font-semibold tracking-tighter">
+          Udeet Mittal
+          <BadgeCheck
+            size={16}
+            strokeWidth={2}
+            fill="#1D9BF0"
+            className="stroke-[#fffaf1d0] dark:stroke-[#070707c4]"
+          />
+          <span className="absolute left-1/2 -translate-x-1/2 bottom-full mb-1 opacity-0 group-hover:opacity-100 group-hover:translate-y-[-4px] items-center justify-center text-[#0f0f0f] bg-[#D1E5F4] text-xs px-3 py-1 rounded shadow-md whitespace-nowrap transition-all duration-300 ease-in-out">
+            meaning 'the rising sun'
+          </span>
+        </span>
 
         {/* Image with mesh blur on hover */}
         <div className="relative w-40 h-40 mb-6 rounded-lg overflow-hidden">
@@ -33,8 +45,8 @@ export default function Page() {
         </div>
 
         <p className="mb-4">
-          {`I'm `}
-          <span className="relative group inline-block">
+          {/* {`I'm `} */}
+          {/* <span className="relative group inline-block">
             <span
               className="font-semibold px-1 rounded-md"
               style={{
@@ -44,26 +56,36 @@ export default function Page() {
                 borderRadius: "4px", // Rounded edges similar to the image
               }}
             >
-              Udeet Mittal
-            </span>
+              Udeet
+            </span> */}
 
-            {/* Tooltip */}
-            <span className="absolute left-1/2 -translate-x-1/2 bottom-full mb-1 opacity-0 group-hover:opacity-100 group-hover:translate-y-[-4px] items-center justify-center text-[#0f0f0f] bg-[#D1E5F4] text-xs px-3 py-1 rounded shadow-md whitespace-nowrap transition-all duration-300 ease-in-out">
-              meaning 'the rising sun'
-            </span>
-          </span>
-          {`, and this is my place on the internet.
+          {/* Tooltip */}
+          {/* </span> */}
+          {`Welcome to my place on the internet.
          Your typical software engineer by profession, but a not-so-typical one by passion.
-        I believe my laziness drives me to automate things, my curiosity drives me to learn new things, all at a fast pace with an intense focus on attention to detail,
-    and I think this is what sets me apart from the crowd. `}
+        I believe my laziness drives me to automate things, my curiosity drives me to learn new things, all at a fast pace with an intense focus on `}
+          <span
+            className="font-medium tracking-tighter"
+            style={{
+              backgroundColor: "#9eff9ec0", // Light green background color
+              color: "#0f0f0f", // Text color
+              padding: "0 4px", // Add padding for better appearance
+              borderRadius: "4px", // Rounded edges similar to the image
+            }}
+          >
+            attention to detail
+          </span>
+          {` and I think this is what sets me apart from the crowd.`}
         </p>
         <p className="mb-4">
-          {`Though having worked most extensively in full stack development and data analytics, I've come to realise that the domain and specific technologies I work in don't really matter—the problem statement in front of me and the people I work with do.`}
+          {`Though having worked most extensively in full stack development and data analytics, I've come to realise that the domain and specific technologies I work in don't`}
+          <em> really </em>
+          {`matter—the problem statement in front of me and the people I work with do.`}
         </p>
         <p className="mb-4">
           {`I'm currently looking for my next role as a `}
           <span
-            className="font-semibold"
+            className="font-medium tracking-tighter"
             style={{
               backgroundColor: "#FFC78F", // Light green background color
               color: "#0f0f0f", // Keep the text color unchanged
@@ -74,7 +96,7 @@ export default function Page() {
             Software Engineer
           </span>
           {`, having industry experience through 4 internships + 1 full-time role.
-        Feel free to reach out at any of the socials below.`}
+     Feel free to reach out at any of the socials or leave a comment below.`}
         </p>
 
         {/* <div className="my-8">
